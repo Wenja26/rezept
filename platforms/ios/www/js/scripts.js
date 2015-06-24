9ed30e59fc455b71c2966e9baabef556415053f0
Wenja26 <Wenia@Wenjas-MacBook-Air.local>
2015-06-24 17:58:35 +0200

    zubereitung

--- platforms/ios/www/js/scripts.js
+++ platforms/ios/www/js/scripts.js
@@ -5,5 +5,17 @@ function startApp() {
 	// Verbindung zur Datenbank öffnen
 	//openDB();
 	
+	$(uebersicht).center();
 	
 }
+
+
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
+
